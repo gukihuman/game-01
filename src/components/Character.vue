cb58c4fc5252bc71c7dfc596b8bf0ca27f7d95d8
ivan-f-sokolov <sokolovivanf@gmail.com>
2022-08-16 20:16:30 +0500

    Add enemy and character

---
+++ src/components/Character.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  inject: ["provider"],
+  props: ["x1", "x2", "radius"],
+  render() {
+    if (!this.provider.context) return;
+
+    const c = this.provider.context;
+
+    c.beginPath();
+    c.arc(this.x1, this.x2, this.radius, 0, Math.PI * 2, false);
+    c.fillStyle = "#a2d2ff";
+    c.fill();
+  },
+};
+</script>
--- src/views/GameView.vue
+++ src/views/GameView.vue
@@ -1,7 +1,9 @@
 <template lang="pug">
 
 Canvas(class="bg-slate-800 w-screen h-screen" ref='canvas')
-  Village(:x1='x1', :x2='x2' :radius='100')
+  Village(:x1='center.x1', :x2='center.x2' :radius='villageRadius')
+  Enemy(:x1='enemyPosition.x1', :x2='enemyPosition.x2', :radius='enemyRadius')
+  Character(:x1='enemyAngle().x1', :x2='enemyAngle().x2', :radius='characterRadius')
 
 
 </template>
@@ -9,21 +11,50 @@ Canvas(class="bg-slate-800 w-screen h-screen" ref='canvas')
 <script>
 import Village from "@/components/Village.vue";
 import Canvas from "@/components/Canvas.vue";
+import Enemy from "@/components/Enemy.vue";
+import Character from "@/components/Character.vue";
 
 export default {
   components: {
     Village,
     Canvas,
+    Enemy,
+    Character,
   },
   data() {
     return {
-      x1: null,
-      x2: null,
+      center: {
+        x1: null,
+        x2: null,
+      },
+      villageRadius: 130,
+      enemyPosition: {
+        x1: 500,
+        x2: 1000,
+      },
+      enemyRadius: 20,
+      characterRadius: 30,
     };
   },
+  methods: {
+    enemyAngle() {
+      const legH = this.center.x1 - this.enemyPosition.x1;
+      const legV = this.center.x2 - this.enemyPosition.x2;
+      const hypo = Math.sqrt(legH ** 2 + legV ** 2);
+      const ratio = this.villageRadius / hypo;
+      const newLegH = legH * ratio;
+      const newLegV = legV * ratio;
+      const x1 = this.center.x1 - newLegH;
+      const x2 = this.center.x2 - newLegV;
+      return { x1, x2 };
+    },
+  },
   mounted() {
-    this.x1 = this.$el.clientWidth / 2;
-    this.x2 = this.$el.clientHeight / 2;
+    this.center.x1 = this.$el.clientWidth / 2;
+    this.center.x2 = this.$el.clientHeight / 2;
+    // setInterval(() => {
+    //   this.enemyPosition.x1 += 10;
+    // }, 1000 / 60);
   },
 };
 </script>
